96d8fde2c1fdb7d8cc5f231d92031d3616be9793
Matthew Wynn <matthew@matthewwynn.com>
2019-02-03 20:55:20 -0600

    Start frontend

---
+++ web/src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Placeholder } from "semantic-ui-react";
+
+class App extends Component {
+  render() {
+    return (
+      <Placeholder>
+        <Placeholder.Header image>
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Header>
+        <Placeholder.Paragraph>
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Paragraph>
+        <Placeholder.Paragraph>
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Paragraph>
+      </Placeholder>
+    );
+  }
+}
+
+export default App;
