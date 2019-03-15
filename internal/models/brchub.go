1b1ac3ce0d91ffd63cba8e400c1b0a0f1899bf4a
Michael Aldridge <aldridge.mac@gmail.com>
2019-03-14 21:09:30 -0700

    internal/models: Add BRCHub

---
+++ internal/models/brchub.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"net/url"
+)
+
+// A BRCHub is a hub that is hosting a season of the BEST Robotics
+// Competition.
+type BRCHub struct {
+	// ID as an integer for convenience.
+	ID int `storm:"index,increment"`
+
+	// This hub is owned by a normal hub.
+	Hub Hub
+
+	// BRC hubs are seasonal.
+	Season Season
+
+	// Slice of events as a hub may have more than the minimum, so
+	// we give them a slice for convenience.
+	Events []Event
+
+	// Assorted metadata about the BRCHub.  This is where all
+	// kinds of fun information lives that tracks the hub's
+	// participation in a season.
+	Meta struct {
+		BRIApproved bool
+		Sponsors []struct{
+			Name string
+			Logo url.URL
+		}
+	}
+}
